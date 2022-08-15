45e4f21683d5094fe427e47e8cc7476437650b5e
Juliano Alessandro dos Santos <111141842+julianoAlessandro@users.noreply.github.com>
2022-08-14 22:21:14 -0300

    Add files via upload

---
+++ Corretor de questoes.py
@@ -0,0 +1,100 @@
+A = 0
+B = 1
+C = 2
+D = 3
+print("=========Informe suas respostas===================")
+print(" correspondencia: A = 0 B = 1 C = 2 D = 3 ")
+Q1 = int(input("Informe a resposta da Questão : "))
+Q2 = int(input("Informe a resposta da Questão : "))
+Q3 = int(input("Informe a resposta da Questão : "))
+Q4 = int(input("Informe a resposta da Questão : "))
+Q5 = int(input("Informe a resposta da Questão : "))
+Q6 = int(input("Informe a resposta da Questão : "))
+Q7 = int(input("Informe a resposta da Questão : "))
+Q8 = int(input("Informe a resposta da Questão : "))
+Q9 = int(input("Informe a resposta da Questão : "))
+Q10 = int(input("Informe a resposta da Questão : "))
+Q11 = int(input("Informe a resposta da Questão : "))
+Q12 = int(input("Informe a resposta da Questão : "))
+
+print("confira a resolução")
+if Q1 == A :
+   print("1)acertou")
+     
+else:
+     print("1)errou")
+
+
+
+if Q2 == B :
+   print("2)acertou")
+      
+else:
+    print("2)errou")
+    
+
+if Q3 == C :
+   print("3)acertou")
+      
+else:
+    print("3)errou")
+    
+if Q4 == D :
+   print("4)acertou")
+      
+else:
+    print("4)errou")
+
+if Q5 == A :
+   print("5)acertou")
+      
+else:
+    print("5)errou")
+    
+if Q6 == D :
+   print("6)acertou")
+      
+else:
+    print("6)errou")
+    
+if Q7 == D :
+   print("7)acertou")
+      
+else:
+    print("7)errou")
+    
+if Q8 == D :
+   print("8)acertou")
+      
+else:
+    print("8)errou")
+    
+if Q9 == D :
+   print("9)acertou")
+      
+else:
+    print("9)errou")
+    
+if Q10 == D :
+   print("10)acertou")
+      
+else:
+    print("10)errou")
+    
+if Q11 == D :
+   print("11)acertou")
+      
+else:
+    print("11)errou")
+    
+    
+if Q12 == D :
+   print("12)acertou")
+      
+else:
+    print("12)errou")
+    
+        
+
+    
+
